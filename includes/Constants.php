913197b1b811dee3b60f3314a8858cc2524ffd95
oscarmg <oscarmg.university@gmail.com>
2019-03-18 09:19:43 +0300

    Update Constants.php

--- includes/Constants.php
+++ includes/Constants.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_USERNAME','joslabs1_oscar');
-define('DB_PASSWORD','f#YsV~w%jaTS');
+define('DB_USERNAME','*****');
+define('DB_PASSWORD','*****');
 define('DB_HOST','localhost');
-define('DB_NAME','joslabs1_acaciaDb');
+define('DB_NAME','*****');
 
 define('USER_CREATED', 101);
 define('USER_EXIST', 102);
